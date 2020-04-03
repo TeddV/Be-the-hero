841c06d02affadf8057e5003f49746e0e1795a12
TeddV <vinicius.b37@outlook.com>
2020-04-03 11:37:41 -0300

    Pagina profile

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './Pages/Logon';
 import Register from './Pages/Register';
+import Profile from './Pages/Profile';
 
 export default function Routes() {
     return(
         <BrowserRouter>
             <Switch>
-                <Route path= "/" exact component = {Logon}/>
-                <Route path= "/register" component = {Register}/>
+                <Route path = "/" exact component = {Logon}/>
+                <Route path = "/register" component = {Register}/>
+                <Route path = "/profile" component = {Profile}/>
 
             </Switch>
         </BrowserRouter>
